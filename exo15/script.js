75543aafac7870e94d694785fd11bf6d06c60243
gibo <bonenfant.gilles@gmail.com>
2021-02-17 16:33:08 +0100

    correction exo 15 : création d'éléments

---
+++ exo15/script.js
@@ -0,0 +1,31 @@
+/*
+on va mettre en place un écouteur d'évènement sur le bouton qui va déclencher la fonction1()
+fonction1() va créer une balise "p" pour y mettre un texte. On va placer cette nouvelle balise dans la div ".target"
+*/
+
+// on sélectionne la balise du bouton
+let buttonElement = document.querySelector('button');
+
+// on lui adjoint un écouteur d'évènements : si on clique sur le bouton, la fonction1() se déclenchera
+buttonElement.addEventListener('click', fonction1)
+
+
+
+
+function fonction1() {
+    //todo on créé la balise <p> avec createElement()
+    let paragrapheElement = document.createElement('p');
+    console.log(paragrapheElement);
+
+    //todo on y ajoute le texte voulu
+    paragrapheElement.innerHTML = "<h1>ceci est un paragraphe.</h1>"
+
+    //todo on sélectionne l'endroit où la placer
+    targetElement = document.querySelector('.target');
+
+    //todo on place la balise créée à l'endroit sélectionné avec appendChild()
+    targetElement.appendChild(paragrapheElement);
+
+
+}
+
